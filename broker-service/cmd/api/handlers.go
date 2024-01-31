8e7192f2acc35b6b2f2f73e5d57c9f943c8af665
kedarnathpc <kedarnathpc2002@gmail.com>
2024-01-31 18:22:31 +0530

    updated

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
-func (app *Config) Broker(rw http.ResponseWriter, r *http.Request) {
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	rw.Header().Set("Content-Type", "application/json")
-	rw.WriteHeader(http.StatusAccepted)
-	rw.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
